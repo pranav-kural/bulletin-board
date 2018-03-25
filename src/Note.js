9b7b4dde66d041dcddf267e4ce6f8776dbcf7430
Pranav Kural <pranavkural042@gmail.com>
2018-03-24 23:03:04 -0400

    Add event handlers to Note

--- src/Note.js
+++ src/Note.js
@@ -1,8 +1,59 @@
-import React, { Component } from 'React'
+import React, { Component } from 'react'
 
 class Note extends Component {
 
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+      editing: false
+    }
+    this.edit = this.edit.bind(this)
+    this.remove = this.remove.bind(this)
+    this.save = this.save.bind(this)
+    this.renderForm = this.renderForm.bind(this)
+    this.renderDisplay = this.renderDisplay.bind(this)
 
+  }
+
+  edit() {
+    this.setState = {
+      editing: true
+    }
+  }
+
+  remove() {
+    alert('note removed')
+  }
+
+  save() {
+    alert('note saved')
+
+    this.setState = {
+      editing: false
+    }
+  }
+
+  renderDisplay() {
+    return (
+      <div className="note">
+        <p>Learn React</p>
+        <span>
+          <button onClick={this.edit}>Edit</button>
+          <button onClick={this.remove}>Remove</button>
+        </span>
+      </div>
+    )
+  }
+
+  renderForm() {
+    // return form
+    return <h1>Form</h1>
+  }
+
+  render() {
+    return (!this.state.editing) ? this.renderDisplay() : this.renderForm();
+  }
 
 }
+
+export default Note
